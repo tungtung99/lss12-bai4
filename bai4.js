fd2f3217842810bc94d6a251ad8fe72a0da35f68
tungtung99 <tungnguyen99hn1@gmail.com>
2020-10-19 21:55:05 +0700

    hoanthanhbaichanle

---
+++ bai4.js
@@ -0,0 +1,9 @@
+function kiemtra(){
+    let so=Number(document.getElementById("inputso").value)
+    if(so%2==0){
+        alert(so+" là một số chẵn")
+    }
+    else{
+        alert(so+ " là một số lẻ")
+    }
+}
